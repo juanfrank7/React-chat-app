47a5e372987267fa447ea1ed4d5f960314ad69ba
Juan F Gonzalez <juano_gonza@hotmail.com>
2018-08-21 12:25:18 -0500

    completed chat functionality

--- src/components/SendMessageForm.js
+++ src/components/SendMessageForm.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
 class SendMessageForm extends React.Component {
   constructor() {
     super();
     this.state = {
-      message: ""
+      message: '',
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -12,7 +12,7 @@ class SendMessageForm extends React.Component {
 
   handleChange(event) {
     this.setState({
-      message: event.target.value
+      message: event.target.value,
     });
   }
 
@@ -20,7 +20,7 @@ class SendMessageForm extends React.Component {
     event.preventDefault();
     this.props.sendMessage(this.state.message);
     this.setState({
-      message: ""
+      message: '',
     });
   }
 
@@ -28,6 +28,8 @@ class SendMessageForm extends React.Component {
     return (
       <form onSubmit={this.handleSubmit} className="send-message-form">
         <input
+          disabled={this.props.disabled}
+          value={this.state.message}
           onChange={this.handleChange}
           placeholder="Type your message and hit Enter"
           type="text"
